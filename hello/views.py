1dd67121506fd0311d11bd5b2bb86eaf07a03ff5
William Costa <wd.costa@campus.fct.unl.pt>
2020-10-15 18:26:32 +0100

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-
-from .models import Greeting
+from models import Greeting
 
 # Create your views here.
 
